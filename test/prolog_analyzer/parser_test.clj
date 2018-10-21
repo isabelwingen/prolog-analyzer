6c2124fa914b48d51fec5cde8001be53ef906a89
Isabel Wingen <isabel.wingen@hhu.de>
2018-10-21 10:21:06 +0200

    Add more tests for parser

--- test/prolog_analyzer/parser_test.clj
+++ test/prolog_analyzer/parser_test.clj
@@ -8,15 +8,46 @@
    "bar(b,X)" [:Fact [:Name "bar"] [:Args [:Atom "b"] [:Var "X"]]]})
 
 
+(deftest parse-empty
+  (testing "Parsing whitespace strings"
+    (are [x] (= '() (sut/parse x))
+      ""
+      "\n"
+      "\n\n"
+      "%Normal Comment\n"
+      "\n%Second Line Comment\n"
+      "         "
+      "\t\t\t")))
 
-(deftest parse-prolog1
+(deftest parse-with-comment
+  (testing "Parsing text with comments"
+    (are [x y] (= x (sut/parse y))
+      '([:Fact [:Name "a"]], [:Fact [:Name "b"]])
+      "a.\n%This is a comment\nb."
+      '([:Fact [:Name "c"]], [:Fact [:Name "d"]], [:Fact [:Name "e"]])
+      "c.%This is a comment at the end of line\n%Another comment\nd.%In-Line Comment\ne.\n\n"
+      '([:Fact [:Name "f"]])
+      "f.\n\n%End of File-Comment")))
+
+(deftest parse-error
+  (testing "Parsing things, that are wrong"
+    (are [x] (contains? (sut/parse x) :reason)
+      "a"
+      "a.b."
+      "a :- "
+      "a :- ."
+      "a(c,d"
+      "a).")))
+
+;; Test for parsing the component
+(deftest parse-facts
   (testing "Simple Facts"
     (are [x y] (= x (sut/prolog-parser y :start :Fact))
       [:Fact [:Name "asdasdasd"]] "asdasdasd."
       [:Fact [:Name "foo"] [:Arglist [:Atom "a"]]] "foo(a)."
       [:Fact [:Name "bar"] [:Arglist [:Atom "b"] [:Var "X"]]] "bar(b,X).")))
 
-(deftest parse-prolog2
+(deftest parse-rules
   (testing "Simple Rules"
     (are [x y] (= x (sut/prolog-parser y :start :Rule))
       [:Rule
@@ -36,7 +67,22 @@
       "foo(A,B) :- a;b,e."
       )))
 
-(deftest parse-prolog3
+(deftest parse-directCall
+  (testing "Parsing of direct calls"
+    (are [x y] (= x (sut/prolog-parser y :start :DirectCall))
+      [:DirectCall
+       [:Goal [:Name "enable_all_specs"]]]
+      ":- enable_all_specs."
+      [:DirectCall
+       [:Goal
+        [:Name "assert"]
+        [:Arglist
+         [:Compound
+          [:Functor "foo"] [:Arglist [:Atom "a"] [:Atom "b"]]]]]]
+      ":- assert(foo(a,b))."
+      )))
+
+(deftest parse-if
   (testing "If"
     (are [y x] (= x (sut/prolog-parser y :start :If))
       "(a -> c ; d)"
