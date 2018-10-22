9f0644247f54fa8bf090889c298ed9262389b644
Isabel Wingen <isabel.wingen@hhu.de>
2018-10-22 20:25:46 +0200

    Add clojure/tools.logging to project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [instaparse "1.4.9"]]
+                 [instaparse "1.4.9"]
+                 [org.clojure/tools.logging "0.4.1"]]
   :main ^:skip-aot prolog-analyzer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
