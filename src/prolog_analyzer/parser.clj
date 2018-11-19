cb60ec3a20d6e035b3d29f739906e180d3880d2f
Isabel Wingen <isabel.wingen@hhu.de>
2018-11-19 12:16:20 +0100

    Create some initial code to create and work with edn of a prolog source file

--- src/prolog_analyzer/parser.clj
+++ src/prolog_analyzer/parser.clj
@@ -1,93 +1,26 @@
 (ns prolog-analyzer.parser
   (:require [instaparse.core :as insta]
             [clojure.pprint :refer [pprint]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure.java.shell :as sh]))
 
+;(defn read-prolog-code [file]
+ ; (with-open [in (java.io.PushbackReader. (clojure.java.io/reader file))]
+  ;  (let [edn-seq (repeatedly (partial edn/read {:eof :theend} in))]
+   ;   (dorun (map println (take-while (partial not= :theend) edn-seq))))))
+(defn get-clojure-file-name [file]
+  (str file ".clj"))
 
-(def prolog-parser
+(defn call-swipl [file]
+  (sh/sh "swipl" "-f" file "-q" "-g" "halt."))
 
-  (insta/parser
-   "<S> = <OptionalWs> (RuleWithPoint | Fact | DirectCall | <Comment> | <OptionalWs>) ((<Ws> | <Comment>) S)?
-    <Comment> = <Single-Line-Comment> | <Multi-Line-Comment>
+(defn read-prolog-code [file]
+  (call-swipl file)
+  (with-open [in (java.io.PushbackReader. (clojure.java.io/reader (get-clojure-file-name file)))]
+    (let [edn-seq (repeatedly (partial edn/read {:eof :theend} in))]
+      (dorun (map println (take-while (partial not= :theend) edn-seq))))))
+;; https://stackoverflow.com/questions/15234880/how-to-use-clojure-edn-read-to-get-a-sequence-of-objects-in-a-file
 
-    <RuleWithPoint> = Rule <Period>
-    Rule = Module? Name Arglist? <StartOfBody> Goals
-    Fact = Module? Name Arglist? <Period>
-    DirectCall =  <StartOfBody> (Special | Metapredicate | Goals) <Period>
-    Special = ('dynamic' | 'multifile' | 'public') <Ws> <OptionalWs> Declaration (<Komma> Declaration)*
-    Declaration = Module? Name <'/'> Arity
-    Metapredicate = <'meta_predicate '> <OptionalWs> Metagoal (<Komma> Metagoal)*
-    Metagoal = Module? Name Arglist?
-    Arity = #'[0-9]+'
-    <Comment> = <Single-Line-Comment> <OptionalWs> | <Multi-Line-Comment> <OptionalWs>
-
-    <Goals> = Goal <Comment>* | Goals (Komma | Semicolon | IfArrow) <OptionalWs> Goals | InBrackets
-    Goal = If | Module? Name Arglist? | Cut | True | False | Fail | Not | Assignment | BoolExpr
-    Module = Name <':'>
-    InBrackets = <OpenBracket> Goals <CloseBracket>
-
-    <Assignment> = IsAssignment | UnifyAssignment
-
-    UnifyAssignment = Term <OptionalWs> ('=' | '==') <OptionalWs> Term
-    IsAssignment = Term <OptionalWs> <'is'> <OptionalWs> Expr
-    <Expr> = Var | Number | Expr <Op> Expr | <OpenBracket> Expr <CloseBracket>
-    BoolExpr =  Expr <BoolOp> Expr | <OpenBracket> BoolExpr <CloseBracket>
-    BoolOp = <OptionalWs> ('<' | '>' | '=') <OptionalWs>
-    Op = <OptionalWs> ('+' | '-' | '*' | '**' | '^' | '/') <OptionalWs>
-
-    Arglist = <OpenBracket> Terms <CloseBracket>
-    <Terms> = (Term | Inner) (<Komma> (Term | Inner))*
-    Inner = <OpenBracket> Terms <CloseBracket>
-    <Term> = Number | NotNumber | Rule | Goals
-    <NotNumber> = Var | Atom | Compound | List | String
-    Compound = Functor Arglist | Term SpecialChar_Compound Term | SpecialChar_Compound NotNumber | '\\'' SpecialChar_Compound '\\'' Arglist?
-    List = EmptyList | ExplicitList  | HeadTailList
-    EmptyList = <'['> <']'>
-    HeadTailList = <'['> <OptionalWs> Terms <OptionalWs> <'|'> <OptionalWs> (List | Var) <OptionalWs> <']'>
-    ExplicitList = <'['> <OptionalWs> Terms <OptionalWs> <']'>
-    Cut = <'!'>
-    True = <'true'>
-    False = <'false'>
-    Fail = <'fail'>
-    Repeat = <'repeat'>
-    Not = <'not('> <OptionalWs> Goals <OptionalWs> <')'> | <'\\\\+'> <OptionalWs> Goals
-    If = <OpenBracket> AndListOfGoals Then AndListOfGoals Else AndListOfGoals <CloseBracket>
-    <AndListOfGoals> = Goal | AndListOfGoals Komma AndListOfGoals | InBrackets
-
-    Var = #'[A-Z_][a-zA-Z0-9_]*'
-    String = <'\\''> #'[^\\']*' <'\\''> | <'\\\"'> #'[^\\\"]*' <'\\\"'>
-    Functor = #'[a-z][a-zA-Z0-9_]*'
-    Name = #'[a-z][a-zA-Z0-9_]*' | Var
-    Atom = #'[a-z][a-zA-Z0-9_]*' | SpecialChar_Atom+
-    <Number> = Integer | Float
-    Integer = #'[\\-\\+]?\\s*[0-9]+'
-    Float = #'[\\-\\+]?\\s*[0-9]+\\.[0-9]+'
-    <SpecialChar_Atom> = '$' | '&' | '*' | '+' | '-' | '.' | '/' | ':' | '<' | '>' | '=' | '?' | '@' | '^' | '~'
-    SpecialChar_Compound = <OptionalWs> #'[\\+\\-\\/:]+' <OptionalWs>
-
-    Komma = <OptionalWs> <','> <OptionalWs>
-    Semicolon = <OptionalWs> <';'> <OptionalWs>
-    Period = <OptionalWs> <'.'> <Comment>*
-    OpenBracket = <OptionalWs> <'('> <OptionalWs>
-    CloseBracket = <OptionalWs> <')'> <OptionalWs>
-    StartOfBody = <OptionalWs> <':-'> <OptionalWs>
-    IfArrow = <OptionalWs> <'->'> <OptionalWs>
-    Then = <OptionalWs> <'->'> <OptionalWs>
-    Else = <Semicolon>
-    <Ws> = #'\\s+'
-    <OptionalWs> = (#'\\s*' <Comment>*)*
-    Single-Line-Comment = #'%.*\n'
-    Multi-Line-Comment = #'/\\*+[^*]*\\*+(?:[^/*][^*]*\\*+)*/'
-"
-   :output-format :hiccup))
-
-
-(defn parse [string]
-  (let [result (insta/parse prolog-parser (str string "\n"))]
-    (if (insta/failure? result)
-      (do
-        (log/error result)
-        result)
-      result)
-    ))
-
+(read-prolog-code "prolog/example.pl")
