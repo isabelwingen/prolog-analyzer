16c8d31d80c30f6bf59259fc91fe90f34d17abab
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-01 15:35:48 +0100

    Change in resource files

---
+++ resources/process-file-test.pl
@@ -0,0 +1,26 @@
+:- module(process_file_test,[]).
+:- use_module("../prolog/prolog_analyzer",[enable_write_out/0,spec_pre/2,spec_post/3,spec_invariant/2,define_spec/2,declare_spec/1]).
+
+:- enable_write_out.
+
+
+:- declare_spec(intOrVar).
+:- declare_spec(foo).
+
+:- define_spec(foo,compound(foo(int,int))).
+:- define_spec(intOrVar,one_of([int,var])).
+
+
+:- spec_pre(member_int/2,[int,list(int)]).
+:- spec_pre(member_int/2,[var,list(int)]).
+:- spec_post(member_int/2,[var,list(int)],[int,list(int)]).
+:- spec_invariant(member_int/2,[any,ground]).
+member_int(H,[H,_]) :- !.
+member_int(E,[_,T]) :-
+    member_int(E,T).
+
+
+:- spec_pre(foo/3,[foo,intOrVar,intOrVar]).
+:- spec_post(foo/3,[foo,intOrVar,intOrVar],[foo,int,int]).
+:- spec_post(foo/3,[nonvar,int,int],[foo,int,int]).
+decode_foo(foo(A,B),A,B).
--- resources/simple-example.pl
+++ resources/simple-example.pl
@@ -1,8 +1,13 @@
 :- module(simple_example,[]).
-:- use_module("../prolog/term_expander",[enable_write_out]).
+:- use_module("../prolog/prolog_analyzer",[enable_write_out/0,spec_pre/2,spec_post/3]).
 
 :- enable_write_out.
 
+:- spec_pre(member/2,[int,list(int)]).
+:- spec_pre(member/2,[var,list(int)]).
+:- spec_post(member/2,[var,list(int)],[int,list(int)]).
 member(H,[H|_]).
 member(E,[_|T]) :-
     member(E,T).
+
+
