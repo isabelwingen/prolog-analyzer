842c3fab92e004246d240ee0c3b5de7e37fac055
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-14 16:21:12 +0100

    Add lein-githooks to project
    -
    -
    -

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [instaparse "1.4.9"]
-                 [org.clojure/tools.logging "0.4.1"]]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [lein-githooks "0.1.0"]]
   :main ^:skip-aot prolog-analyzer.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-githooks "0.1.0"]]
+                   :githooks {:auto-install true
+                              :pre-push ["lein test"]
+                              :pre-commit ["lein eastwood"]}}})
