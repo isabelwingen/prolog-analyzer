97ef274d4986b5a288975dc226f492e2121300df
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-15 14:33:46 +0100

    Improve KNF Transformation of domains

--- src/prolog_analyzer/domain.clj
+++ src/prolog_analyzer/domain.clj
@@ -1,7 +1,8 @@
 (ns prolog-analyzer.domain
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
-            [clojure.spec.test.alpha :as stest]))
+            [clojure.spec.test.alpha :as stest]
+            ))
 
 
 (s/def :normal/spec (s/spec #{:other}))
@@ -34,45 +35,88 @@
     :or :or
     :normal))
 
+(defn- knf-inner? [dom]
+  (case (spec-type dom)
+    :normal true
+    :and (every? knf-inner? (:arglist dom))
+    :or false))
+
+(defn knf? [dom]
+  (case (spec-type dom)
+    :normal true
+    :and false
+    :or (every? knf-inner? (:arglist dom))))
+
 (defmulti to-knf spec-type)
 
 (defmethod to-knf :normal [dom]
-  {:spec :or :arglist [dom]})
-
-(defmethod to-knf :or [dom]
   dom)
 
+(defmethod to-knf :or [dom]
+  (if (= 1 (count (distinct (:arglist dom))))
+    (to-knf (first (:arglist dom)))
+    (-> dom
+        (update :arglist distinct)
+        (update :arglist (partial map to-knf))
+        (update :arglist (partial mapcat #(get % :arglist (list %))))
+        (update :arglist distinct)
+        )))
+
+(defn and-two-doms [a b]
+  (let [new-dom (cond
+                  (and (= :and (:spec a)) (= :and (:spec b))) (update a :arglist (partial concat (:arglist b)))
+                  (= :and (:spec a)) (update a :arglist #(conj % b))
+                  (= :and (:spec b)) (update b :arglist #(conj % a)))]
+    (update new-dom :arglist distinct)
+    ))
+
+
 (defmethod to-knf :and [dom]
-  (if (= 1 (count (:arglist dom)))
-    (assoc dom :spec :or)
+  (if (= 1 (count (distinct (:arglist dom))))
+    (to-knf (first (:arglist dom)))
     (-> dom
         (update :arglist #(->> %
                                (map to-knf)
-                               (map :arglist)
+                               (map (fn [dom] (get dom :arglist [dom])))
                                (reduce (fn [left right]
                                          (for [a left
                                                b right]
-                                           {:spec :and :arglist [a b]})))))
+                                           (cond
+                                             (= a b) a
+                                             (or (= :and (:spec a)) (= :and (:spec b))) (and-two-doms a b)
+                                             :else {:spec :and :arglist [a b]}))))))
+        (update :arglist distinct)
         (assoc :spec :or))))
+
+(binding [s/*recursion-limit* 1] (gen/generate (s/gen (s/and ::dom knf?))))
+
+(s/fdef simplify-knf
+        :args (s/cat :dom ::dom)
+        :ret (s/and ::dom)
+        :fn #(knf? (:ret %)))
+
+(defn simplify-dom [dom]
+  (-> dom
+      to-knf)
+  )
 
 
 
 (defn merge-dom [dom1 dom2]
-  (to-knf {:spec :and :arglist (list dom1 dom2)}))
+  (simplify-dom {:spec :and :arglist (list dom1 dom2)}))
 
 
 (defn union [dom1 dom2]
   (cond
     (is-subdom? dom1 dom2) dom2
     (is-subdom? dom2 dom1) dom1
-    :else (to-knf {:spec :or :arglist (list dom1 dom2)})))
+    :else (simplify-dom {:spec :or :arglist (list dom1 dom2)})))
 
 
 (defn intersect [dom1 dom2]
   (cond
     (is-subdom? dom1 dom2) dom1
     (is-subdom? dom2 dom1) dom2
-    :else (to-knf {:spec :and :arglist (list dom1 dom2)})))
+    :else (simplify-dom {:spec :and :arglist (list dom1 dom2)})))
 
 
-(to-knf {:spec :and :arglist [{:spec :or :arglist [{:spec :a} {:spec :b}]} {:spec :or :arglist [{:spec :c} {:spec :d}]}]})
--- test/prolog_analyzer/domain_test.clj
+++ test/prolog_analyzer/domain_test.clj
@@ -5,6 +5,8 @@
             [clojure.spec.gen.alpha :as gen]
             [clojure.test.check :as tc]
             [clojure.test.check.properties :as prop]
+
+            [clojure.spec.test.alpha :as stest]
             ))
 
 (s/def :simple/arglist (s/and (s/coll-of ::simple-dom) #(< (count %) 4)))
@@ -19,6 +21,43 @@
 
 (deftest to-knf-test
   (are [input expected] (= expected (sut/to-knf input))
-    {:spec :a} {:spec :or :arglist [{:spec :a}]}
-    {:spec :or :arglist [{:spec :a}]} {:spec :or :arglist [{:spec :or :arglist [{:spec :a}]}]}
+    {:spec :a} {:spec :a}
+    {:spec :or :arglist [{:spec :a}]} {:spec :a}
+    {:spec :and :arglist [{:spec :a}]} {:spec :a}
+    {:spec :and :arglist [{:spec :a} {:spec :a}]} {:spec :a}
+    {:spec :or :arglist [{:spec :a} {:spec :b} {:spec :a}]} {:spec :or :arglist [{:spec :a} {:spec :b}]}
+
+    ;; (a or b) or (b or c) == a or b or c
+    {:spec :or :arglist [{:spec :or :arglist [{:spec :a} {:spec :b}]}
+                         {:spec :or :arglist [{:spec :b} {:spec :c}]}]}
+    {:spec :or :arglist [{:spec :a} {:spec :b} {:spec :c}]}
+
+    ;; (a or b) and (b or c)
+    {:spec :and :arglist [{:spec :or :arglist [{:spec :a} {:spec :b}]}
+                         {:spec :or :arglist [{:spec :b} {:spec :c}]}]}
+    {:spec :or :arglist [{:spec :and :arglist [{:spec :a} {:spec :b}]}
+                         {:spec :and :arglist [{:spec :a} {:spec :c}]}
+                         {:spec :b}
+                         {:spec :and :arglist [{:spec :b} {:spec :c}]}
+                         ]}
+
+    {:spec :and :arglist [{:spec :a} {:spec :b} {:spec :a}]} {:spec :or :arglist [{:spec :and :arglist [{:spec :a} {:spec :b}]}]}
     ))
+
+(deftest knf-test
+  (are [x] (sut/knf? (sut/to-knf x))
+    {:spec :a}
+    {:spec :or :arglist [{:spec :a} {:spec :b}]}
+    {:spec :or :arglist [{:spec :and :arglist [{:spec :a} {:spec :b}]} {:spec :c}]}
+    ))
+
+
+(s/fdef to-knf-wrapper
+        :args (s/cat :dom ::simple-dom)
+        :ret ::sut/dom
+        :fn (s/and #(sut/knf? (:ret %))))
+
+(defn to-knf-wrapper [dom]
+  (sut/to-knf dom))
+
+;(binding [s/*recursion-limit* 1] (stest/abbrev-result (first (stest/check `to-knf-wrapper))))
