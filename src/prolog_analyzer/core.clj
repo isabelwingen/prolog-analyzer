67add02c0fe029f5e4311c02bfb9cc523d360860
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-17 13:53:29 +0100

    Add analysis to main method

--- src/prolog_analyzer/core.clj
+++ src/prolog_analyzer/core.clj
@@ -1,10 +1,14 @@
 (ns prolog-analyzer.core
   (:gen-class)
   (:require [prolog-analyzer.parser :as parser]
+            [prolog-analyzer.analyzer :as analyzer]
             [clojure.pprint :refer [pprint]]))
 
 (defn -main
   "Start analyzing of source file"
   [file]
-  (doseq [elem (parser/process-prolog-file file)]
-    (pprint elem)))
+  (-> file
+      parser/process-prolog-file
+      analyzer/complete-analysis))
+
+(-main "resources/spec-test.pl")
