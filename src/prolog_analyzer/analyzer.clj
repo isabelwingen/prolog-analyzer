bcbd2511f124da0a0b49706db12d3d651839aa01
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-22 12:09:50 +0100

    Add pretty printer
    (cherry picked from commit 9703dd05257830a4c7fc17d86b248772cdf63f9c)

--- src/prolog_analyzer/analyzer.clj
+++ src/prolog_analyzer/analyzer.clj
@@ -145,5 +145,19 @@
         pre-spec (:pre-specs (get-specs-of-pred pred-id))]
     (analyzing impl pre-spec)))
 
+(defmulti to-string :type)
+(defmethod to-string :var [{n :name}] n)
+(defmethod to-string :anon_var [{n :name}] n)
+(defmethod to-string :head-tail-list [{head :head tail :tail}]
+  (str "[" (to-string head) "|" (to-string tail) "]"))
+(defmethod to-string :default [arg] arg)
 
 
+(defn to-pretty-map [{id-mapping :id-mapping :as result}]
+  (let [cut-result (-> result (dissoc :id-mapping) (dissoc :args))]
+    (reduce-kv
+     (fn [m k {dom :dom relations :relations}]
+       (let [new-rel (map (fn [r] (reduce-kv #(assoc %1 %2 (to-string (get id-mapping %3))) {} r)) relations)]
+         (assoc m (to-string (get id-mapping k)) {:dom dom :relations new-rel})))
+     {}
+     cut-result)))
