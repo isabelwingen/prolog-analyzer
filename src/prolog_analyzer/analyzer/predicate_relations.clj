9edeb6557a47e9a016325241ecb3e837be33f2be
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-23 21:11:42 +0100

    Add a ns for call relations between predicates

---
+++ src/prolog_analyzer/analyzer/predicate_relations.clj
@@ -0,0 +1,5 @@
+(ns prolog-analyzer.analyzer.predicate-relations
+  (:require [prolog-analyzer.parser :refer [process-prolog-file process-prolog-snippets]]
+            [prolog-analyzer.analyzer.core :refer [data]]
+            [clojure.pprint :refer [pprint]]))
+
