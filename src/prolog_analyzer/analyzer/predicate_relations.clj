74fd450ce7b5fb96f9a5d02bc1a299ef1239d885
Isabel Wingen <isabel.wingen@hhu.de>
2018-12-29 15:39:23 +0100

    Add ubergraph to display relations between predicates

--- src/prolog_analyzer/analyzer/predicate_relations.clj
+++ src/prolog_analyzer/analyzer/predicate_relations.clj
@@ -2,6 +2,7 @@
   (:require [prolog-analyzer.parser :refer [process-prolog-file process-prolog-snippets]]
             [prolog-analyzer.analyzer.core :refer [data]]
             [prolog-analyzer.utils :as utils]
+            [ubergraph.core :as uber]
             [clojure.pprint :refer [pprint]]))
 
 (def graph (atom {}))
@@ -9,26 +10,24 @@
 (defn add-predicates-to-graph []
   (doseq [pred-id (utils/get-pred-identities @data)
           clause-key (keys (get-in (:preds @data) pred-id))]
-    (if (contains? @graph pred-id)
-      (swap! graph update pred-id #(conj % (conj pred-id clause-key)))
-      (swap! graph assoc pred-id [(conj pred-id clause-key)])))
+    (swap! graph uber/add-directed-edges [pred-id (conj pred-id clause-key)]))
   @graph)
 
+
 (defn add-clauses-to-graph []
-  (doseq [[source-module & _ :as clause-key] (utils/get-clause-identities @data)
-          {goal-name :goal arity :arity goal-module :module :as goal} (get-in (:preds @data) (conj clause-key :body))]
-    (let [goal-pred [goal-module goal-name arity]]
-      (if (contains? @graph clause-key)
-        (swap! graph update clause-key #(conj % goal-pred))
-        (swap! graph assoc clause-key [goal-pred])))
-    )
+  (doseq [[source-module & _ :as clause-id] (utils/get-clause-identities @data)]
+    (let [goals (map (juxt :module :goal :arity) (get-in (:preds @data) (conj clause-id :body)))]
+      (doseq [goal-number (range 0 (count goals))]
+        (swap! graph uber/add-directed-edges [clause-id (nth goals goal-number) {:position goal-number}]))))
   @graph)
 
 (defn create-graph []
-  (reset! graph {})
+  (reset! graph (uber/digraph))
   (add-predicates-to-graph)
   (add-clauses-to-graph)
   )
 
-(pprint (create-graph))
-@graph
+(-> "resources/module1.pl" process-prolog-file prolog-analyzer.analyzer.core/complete-analysis)
+(uber/pprint (create-graph))
+
+
