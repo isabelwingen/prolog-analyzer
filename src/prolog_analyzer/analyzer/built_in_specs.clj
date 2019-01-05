94701c4a437f52da7f03e8948535e7398c3b3fc5
Isabel Wingen <isabel.wingen@hhu.de>
2019-01-05 17:24:48 +0100

    Add built-in specs for is/2 and append/3

--- src/prolog_analyzer/analyzer/built_in_specs.clj
+++ src/prolog_analyzer/analyzer/built_in_specs.clj
@@ -1,2 +1,35 @@
 (ns prolog-analyzer.analyzer.built-in-specs)
 
+(def built-in-specs
+  {"member"
+   {2
+    {:pre-specs [[{:spec :any} {:spec :list :type {:spec :any}}]
+                 [{:spec :var} {:spec :list :type {:spec :any}}]
+                 [{:spec :any} {:spec :var}]
+                 [{:spec :var} {:spec :var}]]
+     :post-specs [[[{:spec :nonvar} {:spec :list :type {:spec :nonvar}}] [{:spec :nonvar} {:spec :list :type {:spec :nonvar}}]]
+                  [[{:spec :var} {:spec :list :type {:spec :named-any :name "X"}}] [{:spec :named-any :name "X"} {:spec :list :type {:spec :named-any :name "X"}}]]
+                  [[{:spec :named-any :name "X"} {:spec :var}] [{:spec :named-any :name "X"} {:spec :list :type {:spec :or :arglist [{:spec :named-any :name "X"} {:spec :var}]}}]]
+                  [[{:spec :var} {:spec :var}] [{:spec :var} {:spec :list :type {:spec :var}}]]]
+     :inv-specs []}}
+   "is"
+   {2
+    {:pre-specs [[{:spec :var} {:spec :expr}]
+                 [{:spec :number} {:spec :expr}]]
+     :post-specs [[[{:spec :any} {:spec :expr}] [{:spec :number} {:spec :expr}]]]
+     :inv-specs []}}
+   "append"
+   {3
+    {:pre-specs [[{:spec :var} {:spec :var} {:spec :var}]
+                 [{:spec :var} {:spec :var} {:spec :list :type {:spec :any}}]
+                 [{:spec :var} {:spec :var} {:spec :list :type {:spec :any}}]
+                 [{:spec :list :type {:spec :any}} {:spec :var} {:spec :var}]
+                 [{:spec :var} {:spec :list :type {:spec :any}} {:spec :list :type {:spec :any}}]
+                 [{:spec :list :type {:spec :any}} {:spec :var} {:spec :list :type {:spec :any}}]
+                 [{:spec :list :type {:spec :any}} {:spec :list :type {:spec :any}} {:spec :var}]
+                 [{:spec :list :type {:spec :any}} {:spec :list :type {:spec :any}} {:spec :list :type {:spec :any}}]]
+     :post-specs [[[{:spec :any} {:spec :any} {:spec :list :type {:spec :named-any :name "X"}}] [{:spec :list :type {:spec :named-any :name "X"}} {:spec :list :type {:spec :named-any :name "X"}} {:spec :list :type {:spec :named-any :name "X"}}]]
+                  [[{:spec :any} {:spec :any} {:spec :any}] [{:spec :list :type {:spec :named-any :name "X"}} {:spec :list :type {:spec :named-any :name "X"}} {:spec :list :type {:spec :named-any :name "X"}}]]]
+     :inv-specs []}}})
+
+;:specs {{:spec "expr"} {:spec :one_of :arglist [{:spec :number} {:spec :compound :arglist [{:spec "expr"}]} {:spec :compound :arglist [{:spec "expr"} {:spec "expr"}]}]}}
