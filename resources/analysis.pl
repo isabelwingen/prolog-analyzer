0d28cf71fb74c2a2ce0a97f87c58890cf0388fb2
Isabel Wingen <isabel.wingen@hhu.de>
2019-01-12 11:15:36 +0100

    Add constructed example good for analysis

---
+++ resources/analysis.pl
@@ -0,0 +1,18 @@
+:- module(analysis,[]).
+:- use_module("../prolog/prolog_analyzer",[enable_write_out/0,spec_pre/2,spec_post/3,spec_invariant/2,define_spec/2,declare_spec/1]).
+
+:- enable_write_out.
+
+
+foo(X,Y) :-
+    bar(X,Y),
+    int(X).
+
+:- spec_pre(bar/2,[int,int]).
+:- spec_pre(bar/2,[atom,atom]).
+bar(X,X).
+
+:- spec_pre(int/1,[int]).
+int(X) :-
+    integer(X).
+
