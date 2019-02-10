205fe596c2e5f3fd74d140c48da660b70da528b5
Isabel Wingen <isabel.wingen@hhu.de>
2019-02-10 14:56:43 +0100

    Clean up utils.clj: Add docstrings and test, remove junk

--- src/prolog_analyzer/utils.clj
+++ src/prolog_analyzer/utils.clj
@@ -1,4 +1,5 @@
 (ns prolog-analyzer.utils
+  "Contains usefull utility functions used across different namespaces."
   (:require [prolog-analyzer.analyzer.built-in-specs :as built-ins]
             [ubergraph.core :as uber]
             [ubergraph.protocols]
@@ -8,8 +9,9 @@
 
 ;; for data extracted from a prolog file
 
-
-(defn get-specs-of-pred [[module pred-name arity :as pred-identity] data]
+(defn get-specs-of-pred
+  "Returns the pre, post and invariant specs of a given `pred-identity` loaded in `data`."
+  [[module pred-name arity :as pred-identity] data]
   (if (= :built-in module)
     (built-ins/get-specs-of-built-in-pred pred-name arity)
     (-> data
@@ -19,77 +21,69 @@
         (update :inv-specs #(get-in % pred-identity))
         )))
 
-(defn get-pred-identities [data]
+(defn get-pred-identities
+  "Returns the predicate ids of all predicates loaded in `data`."
+  [data]
   (for [module (keys (:preds data))
         pred-name (keys (get-in data [:preds module]))
         arity (keys (get-in data [:preds module pred-name]))]
     [module pred-name arity]))
 
-(defn get-clause-identities [data]
+(defn get-clause-identities
+  "Returns the clause ids of all clauses loaded in `data`."
+  [data]
   (let [preds (:preds data)]
     (for [pred-id (get-pred-identities data)
           clause-number (keys (get-in preds pred-id))]
       (conj pred-id clause-number))))
 
-(defn get-clause-identities-of-pred [pred-id data]
+(defn get-clause-identities-of-pred
+  "Returns the clause ids of the predicate with id `pred-id` loaded in `data`."
+  [pred-id data]
   (for [clause-number (keys (get-in (:preds data) pred-id))]
     (conj pred-id clause-number)))
 
-(defn get-clause [clause-id data]
+(defn get-clause
+  "Gets the actual code of a clause in `data` with id `clause-id`."
+  [clause-id data]
   (get-in (:preds data) clause-id))
 
-(defn get-clauses-of-pred [pred-identity data]
-  (vals (get-in data (apply vector :preds pred-identity))))
+(defn get-clauses-of-pred
+  "Gets the code of all clauses belonging to the predicate with `pred-identity` loaded in `data`."
+  [pred-identity data]
+  (vals (get-in (:preds data) pred-identity)))
 
-(defn empty-list? [{term :term type :type}]
+(defn empty-list?
+  "Checks if the input is the empty (prolog) list."
+  [{term :term type :type}]
   (and (= type :atomic) (= term "[]")))
 
-
-;; transform head-tail-list
-(defn to-head-tail-list [& specs]
-  (if (empty? specs)
+(defn to-head-tail-list
+  "Transforms a bunch of `terms` to a proper prolog list."
+  [& terms]
+  (if (empty? terms)
     {:term "[]" :type :atomic}
-    {:type :list :head (first specs) :tail (apply to-head-tail-list (rest specs))}))
+    {:type :list :head (first terms) :tail (apply to-head-tail-list (rest terms))}))
 
-(defn to-tuple-spec [& specs]
-  {:spec :tuple :arglist specs})
+(defn to-tuple-spec
+  "Transforms a bunch of `specs` to a tuple spec."
+  [& specs]
+  (if (empty? specs)
+    {:spec :error :reason "Cannot build a tuple with zero arguments"}
+    {:spec :tuple :arglist specs}))
 
-(defn to-or-spec [& specs]
-  {:spec :one-of :arglist specs})
-
-(to-tuple-spec {:spec :integer} {:spec :var})
-
-(defn head-tail-list-to-list [{head :head tail :tail}]
-  (if (= "[]" (:term tail))
-    {:type :list :elements [head]}
-    (let [{rest-args :elements} (head-tail-list-to-list tail)]
-      {:type :list :elements (apply vector head rest-args)})))
+(defn to-or-spec
+  "Transforms a bunch of `specs` to a one-of spec."
+  [& specs]
+  (case (count specs)
+    0 {:spec :error :reason "Cannot build empty one-of"}
+    1 (first specs)
+    {:spec :one-of :arglist specs}))
 
 (defn get-elements-of-list [{head :head tail :tail}]
   (if (empty-list? tail)
     (list head)
     (conj (get-elements-of-list tail) head)))
-
-;; graphs
-(defn transitive-closure [g]
-  (let [tmp-graph (atom g)]
-    (doseq [k (uber/nodes @tmp-graph)
-            i (uber/nodes @tmp-graph)
-            :when (uber/find-edge @tmp-graph i k)]
-      (doseq [j (uber/nodes @tmp-graph)
-              :when (uber/find-edge @tmp-graph k j)]
-        (swap! tmp-graph uber/add-edges [i j])))
-    @tmp-graph))
-
-(defn transitive-closure-with-attr [g attr-map]
-  (let [tmp-graph (atom g)]
-    (doseq [k (uber/nodes @tmp-graph)
-            i (uber/nodes @tmp-graph)
-            :when (uber/find-edge @tmp-graph (-> attr-map (assoc :src i) (assoc :dest k)))]
-      (doseq [j (uber/nodes @tmp-graph)
-              :when (uber/find-edge @tmp-graph k j)]
-        (swap! tmp-graph uber/add-edges [i j])))
-    @tmp-graph))
 
 (defn- merge-attr [attr1 attr2]
   (let [dom1 (:dom attr1)
@@ -97,41 +91,6 @@
     (assoc (merge attr1 attr2)
            :dom
            (apply vector (concat dom1 dom2)))))
-
-
-
-(defn- merge-nodes [env1 env2]
-  (loop [result env1
-         nodes (uber/nodes env2)]
-    (if-let [node (first nodes)]
-      (if (uber/has-node? result node)
-        (recur (uber/set-attrs result node (merge-attr (uber/attrs result node) (uber/attrs env2 node))) (rest nodes))
-        (recur (uber/add-nodes-with-attrs result [node (uber/attrs env2 node)]) (rest nodes)))
-      result)))
-
-(defn- merge-edges [env1 env2]
-  (apply uber/add-edges env1 (uber/edges env2)))
-
-
-(defn merge-envs [env & envs]
-  (reduce
-   merge-edges
-   (reduce merge-nodes env envs)
-   envs))
-
-(defn dom-map-to-env [dom-map]
-  (let [nodes (map #(vector % {:dom (get dom-map %)}) (keys dom-map))]
-    (apply uber/add-nodes-with-attrs (uber/digraph) nodes)))
-
-
-(defn- new-uuid [env]
-  (->> (uber/nodes env)
-       (filter #(= :specvar (:spec %)))
-       (map :name)
-       sort
-       last 
-       (#(if (nil? %) 0 (inc %)))
-       ))
 
 (defn replace-specvar-name-with-value [spec specvar-name replace-value]
   (case (:spec spec)
@@ -160,7 +119,6 @@
 
     spec
     ))
-
 
 (defn find-specvars [spec]
   (case (:spec spec)
--- test/prolog_analyzer/utils_test.clj
+++ test/prolog_analyzer/utils_test.clj
@@ -13,6 +13,14 @@
 
 (def data (parser/process-prolog-file "resources/spec-test.pl"))
 
+(deftest get-specs-of-pred-test
+  (is (= {:pre-specs [[{:spec :integer} {:spec :list :type {:spec :integer}}]
+                      [{:spec :var}, {:spec :list :type {:spec :integer}}]]
+          :post-specs [[[{:spec :var} {:spec :list :type {:spec :integer}}]
+                        [{:spec :integer}, {:spec :list :type {:spec :integer}}]]]
+          :inv-specs [[{:spec :any} {:spec :ground}]]}
+         (sut/get-specs-of-pred ["spec_test" "member_int" 2] data))))
+
 (deftest get-pred-identities-test
   (is (= [["spec_test" "member_int" 2] ["spec_test" "foo" 3]] (sut/get-pred-identities data))))
 
@@ -23,36 +31,48 @@
         ["spec_test" "foo" 3 0]]
        (sut/get-clause-identities data))))
 
+(deftest get-clause-identities-of-pred-test
+  (is (= [["spec_test" "member_int" 2 0] ["spec_test" "member_int" 2 1]]
+         (sut/get-clause-identities-of-pred ["spec_test" "member_int" 2] data)))
+  (is (= [["spec_test" "foo" 3 0]]
+         (sut/get-clause-identities-of-pred ["spec_test" "foo" 3] data))))
+
 (deftest get-clauses-of-pred-test
   (is (= 2 (count (sut/get-clauses-of-pred ["spec_test" "member_int" 2] data))))
   (is (= 1 (count (sut/get-clauses-of-pred ["spec_test" "foo" 3] data)))))
 
-(deftest get-specs-of-pred-test
-  (let [{pre-specs :pre-specs post-specs :post-specs} (sut/get-specs-of-pred ["spec_test" "member_int" 2] data)]
-    (is (= 2 (count pre-specs)))
-    (is (= (list {:spec :integer} {:spec :list :type {:spec :integer}})
-           (first pre-specs)))
-    (is (= (list {:spec :var} {:spec :list :type {:spec :integer}})
-           (second pre-specs)))
-    (is (= 1 (count post-specs)))
-    (is (= (list (list {:spec :var} {:spec :list :type {:spec :integer}})
-                 (list {:spec :integer} {:spec :list :type {:spec :integer}}))
-           (first post-specs)))
-    ))
-
-(deftest empty-list-test
-  (is (true? (sut/empty-list? {:term "[]" :type :atomic})))
+(deftest empty-list?-test
+  (is (true? (sut/empty-list? {:type :atomic :term "[]"})))
+  (is (false? (sut/empty-list? {:type :atomic :term "."})))
+  (is (false? (sut/empty-list? {:type :atom :term "[]"})))
+  (is (false? (sut/empty-list? {:type :list :head {:spec :var :name "X"} :tail {:type :atomic :term "[]"}})))
   (is (false? (sut/empty-list? {:term "[|]" :type :atomic})))
   (is (false? (sut/empty-list? {:term "[]" :type :atom})))
   (is (false? (sut/empty-list? {:type :list :arglist []}))))
 
-(deftest head-tail-list-to-list-test
-  (is (= {:type :list :elements [{:type :integer :value 1} {:type :integer :value 2}]}
-         (sut/head-tail-list-to-list {:type :list
-                                      :head {:type :integer :value 1}
-                                      :tail {:type :list
-                                             :head {:type :integer :value 2}
-                                             :tail {:term "[]" :type :atomic}}}))))
+(deftest to-head-tail-list
+  (are [x y] (= x (apply sut/to-head-tail-list y))
+    {:type :atomic :term "[]"} []
+
+    {:type :list :head {:type :integer :value 1} :tail {:type :atomic :term "[]"}} [{:type :integer :value 1}]
+
+    {:type :list
+     :head {:type :integer :value 1}
+     :tail {:type :list :head {:type :integer :value 2} :tail {:type :atomic :term "[]"}}} [{:type :integer :value 1} {:type :integer :value 2}]
+    ))
+
+(deftest to-tuple-spec-test
+  (are [x y] (= x (apply sut/to-tuple-spec y))
+    {:spec :error :reason "Cannot build a tuple with zero arguments"} []
+    {:spec :tuple :arglist [{:spec :integer}]} [{:spec :integer}]
+    {:spec :tuple :arglist [{:spec :integer} {:spec :atom}]} [{:spec :integer} {:spec :atom}]))
+
+(deftest to-or-spec-test
+  (are [x y] (= x (apply sut/to-or-spec y))
+    {:spec :error :reason "Cannot build empty one-of"} []
+    {:spec :integer} [{:spec :integer}]
+    {:spec :one-of :arglist [{:spec :integer} {:spec :atom}]} [{:spec :integer} {:spec :atom}]))
+
 
 (deftest get-elements-of-list-test
   (is (= (list {:type :integer :value 1} {:type :integer :value 2} {:type :integer :value 3})
@@ -79,9 +99,3 @@
 (def graph-gen
   (gen/fmap #(apply uber/digraph %) edge-gen))
 
-
-(def transitiv-closure-prop
-    (prop/for-all [g graph-gen]
-                  (= (transitive-closure-ref-impl g) (sut/transitive-closure g))))
-
-(tc/quick-check 500 transitiv-closure-prop)
