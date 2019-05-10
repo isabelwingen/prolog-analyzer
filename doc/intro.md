6af05b7f2a1a74592d6a973809c188b8433eef80
Isabel Wingen <isabel.wingen@hhu.de>
2019-05-10 18:08:20 +0200

    Clean up

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to prolog-analyzer
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/prolog_analyzer/tools/execute_analysis.clj
@@ -0,0 +1,30 @@
+(ns prolog-analyzer.tools.execute-analysis
+  (:require
+   [clojure.java.io :as io]))
+
+
+(defn psize [f]
+  (if (.isDirectory f)
+    (apply + (pmap psize (.listFiles f)))
+    (.length f)))
+
+
+(defn sort-asc [dirs limit]
+  (->> dirs
+       (map #(hash-map :size (psize %) :dir %))
+       (sort-by :size)
+       (take-while #(< (:size %) limit))))
+
+(defn execute-run [dir f]
+  (try
+    (do
+      (f dir)
+      (str dir ": ok\n"))
+    (catch Exception e (str dir ": " (.getMessage e) "\n"))))
+
+
+(defn test-dir [dir limit func]
+  (let [subdirs (.listFiles (io/file dir))
+        res (apply vector (for [f (sort-asc subdirs limit)]
+                            (execute-run (.getAbsolutePath (:dir f)) func)))]
+    res))
