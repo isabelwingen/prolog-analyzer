ef8fd32871c1698081c4a4bf643e7a66f378aa27
Isabel Wingen <isabel.wingen@hhu.de>
2019-05-15 09:44:52 +0200

    Improve type-analysis tools

--- src/prolog_analyzer/tools/type_analyzer.clj
+++ src/prolog_analyzer/tools/type_analyzer.clj
@@ -30,15 +30,6 @@
       (float (/ (+ anys nils) total)))))
 
 
-(defn analysis [file]
-  (->> (get-data file)
-       (remove #(and (string? %) (not= "Call prolog" %)))
-       (partition-by string?)
-       (partition 2)
-       (map second)
-       (map percent)
-       ))
-
 
 (defn mean [l]
   (let [c (count l)
@@ -46,33 +37,27 @@
     (float (/ sum c))))
 
 
+(defn- psize [f]
+  (if (.isDirectory f)
+    (apply + (pmap psize (.listFiles f)))
+    (if (.endsWith (.getName f) ".pl")
+      (.length f)
+      0)))
 
-(defn compare [l1 l2 index]
-  (let [res1 (nth l1 index)
-        res2 (nth l2 index)
-        perc1 (percent res1)
-        perc2 (percent res2)
-        diff (- perc1 perc2)]
-    (when (not= perc1 perc2)
-      (print index ": ")
-      (println diff))
-    ;(clojure.pprint/pprint res1)
-    ;(clojure.pprint/pprint res2)
-    ))
+(defn- sort-asc [dirs limit]
+  (->> dirs
+       (map #(hash-map :size (psize %) :dir %))
+       (sort-by :size)
+       (take-while #(< (:size %) limit))))
 
-(defn compare2 [index]
-  (let [l1 (analysis "results/type-analysis4")
-        l2 (analysis "results/type-analysis3")
-        k1 (map (comp percent vector) (nth l1 14))
-        k2 (map (comp percent vector) (nth l2 14))
-        m1 (nth (nth l1 14) 6)
-        m2 (nth (nth l2 14) 6)]
-    (doseq [x (keys m1)]
-      (clojure.pprint/pprint x)
-      (clojure.pprint/pprint (get m1 x))
-      (clojure.pprint/pprint (get m2 x))
-      (println "-----------------"))
-    ))
-
-(mean (analysis "results/with-lists"))
-(mean (analysis "results/without-lists"))
+(defn analysis [file]
+  (->> (get-data file)
+       (remove #(and (string? %) (not (.startsWith % "/home"))))
+       (partition-by string?)
+       (partition 2)
+       (map #(hash-map :size (first %) :data (second %)))
+       (map (fn [t] (update t :size (partial filter #(.startsWith % "/home")))))
+       (map #(update % :size last))
+       (map #(update % :size (comp psize io/file)))
+       (map #(update % :data percent))
+       ))
