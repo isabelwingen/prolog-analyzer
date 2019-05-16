b8bd414f3c541a68de6912dadcebf7140216fcc8
Isabel Wingen <isabel.wingen@hhu.de>
2019-05-16 09:08:26 +0200

    Improve type analyzer tool

--- src/prolog_analyzer/tools/type_analyzer.clj
+++ src/prolog_analyzer/tools/type_analyzer.clj
@@ -1,7 +1,8 @@
 (ns prolog-analyzer.tools.type-analyzer
   (:require
    [clojure.string]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.set]))
 
 
 (defn get-data [file]
@@ -50,51 +51,91 @@
         (assoc :total total)
         )))
 
+(defn analysis-v2-single-pack [remove-singletons? {filename :filename data :data :as res}]
+  (-> res
+      (update :filename (partial filter #(.startsWith % "/home")))
+      (update :filename last)
+      (update :filename #(.getName (io/file %)))
+      (update :data (partial take-nth 2))
+      (update :data (partial sort-v2 remove-singletons?))
+      (clojure.set/rename-keys {:data :vars})
+      ))
 
-(defn analysis-v2-include-singletons
-  "Can handle print from print-type-and-errors. Do not exclude singletons"
-  [file]
-  (->> (get-data file)
+(defn analysis-v2 [remove-singletons? file]
+  (->> file
+       get-data
        (remove #(and (string? %) (not (.startsWith % "/home"))))
        (partition-by string?)
        (partition 2)
-       (map #(hash-map :size (first %) :data (second %)))
-       (map (fn [t] (update t :size (partial filter #(.startsWith % "/home")))))
-       (map #(update % :size last))
-       (map #(update % :size (comp psize io/file)))
-       (map #(update % :data (partial take-nth 2))) ; filters out errors
-       (map #(update % :data (sort-v2 false)))
+       (map #(hash-map :filename (first %) :data (second %)))
+       (map (partial analysis-v2-single-pack remove-singletons?))
        ))
 
-(defn analysis-v2-exclude-singletons
-  "Exclude singletons."
-  [file]
-  (->> (get-data file)
-       (remove #(and (string? %) (not (.startsWith % "/home")))) ; splits for packages
+(defn prob-type-analysis-v2 [path]
+  (->> path
+       get-data
+       (remove string?)
+       (hash-map :filename ["/home"] :data)
+       (analysis-v2-single-pack false)))
+
+(spit "useful-results/prob-types" "")
+
+(defn analysis-v3-single-pack [remove-singletons? {data :data :as m}]
+  (-> m
+      (update :filename (partial filter #(.startsWith % "/home")))
+      (update :filename last)
+      (update :filename #(.getName (io/file %)))
+      (update :data (partial drop 1))
+      (update :data (partial take-nth 3))
+      (update :data (partial sort-v2 remove-singletons?))
+      (assoc :calls data)
+      (update :calls (partial take-nth 3))
+      (update :calls (partial apply merge-with +'))
+      (clojure.set/rename-keys {:data :vars})
+      ))
+
+(defn analysis-v3
+  "With call counters"
+  [remove-singletons? file]
+  (->> file
+       get-data
+       (remove #(and (string? %) (not (.startsWith % "/home"))))
        (partition-by string?)
        (partition 2)
-       (map #(hash-map :size (first %) :data (second %)))
-       (map (fn [t] (update t :size (partial filter #(.startsWith % "/home")))))
-       (map #(update % :size last))
-       (map #(update % :size (comp psize io/file)))
-       (map #(update % :data (partial take-nth 2))) ; filters out errors
-       (map #(update % :data (sort-v2 true)))
+       (map #(hash-map :filename (first %) :data (second %)))
+       (map (partial analysis-v3-single-pack remove-singletons?))
        ))
 
-(defn percents-of-single-pack [{total :total :as data}]
-  (-> data
-      (update :any #(float (/ % total)))
-      (update :not-any #(float (/ % total)))
-      (update :any-with-compatible #(float (/ % total)))))
+(defn prob-type-analysis-v3 [path]
+  (->> path
+       get-data
+       (remove string?)
+       (hash-map :filename ["/home"] :data)
+       (analysis-v3-single-pack false)))
 
-(defn percents
-  "fn must be analysis-v2 or analysis-v2"
-  [fn file]
-  (->> file
-       fn
-       (map :data)
-       (apply merge-with +')
-       percents-of-single-pack))
+
+(defn percents-of-single-pack [m]
+  (let [total (get-in m [:vars :total])
+        vars (-> m
+                 (update-in [:vars :any] #(float (/ % total)))
+                 (update-in [:vars :not-any] #(float (/ % total)))
+                 (update-in [:vars :any-with-compatible] #(float (/ % total))))
+        total-calls (+ (get-in m [:calls :known] 0) (get-in m [:calls :unknown] 1))]
+    (if (or (contains? (:calls vars) :unknown) (or (contains? (:calls vars) :known)))
+      (-> vars
+          (update-in [:calls :known] #(or % 0))
+          (update-in [:calls :unknown] #(or % 0))
+          (update-in [:calls :known] #(float (/ % total-calls)))
+          (update-in [:calls :unknown] #(float (/ % total-calls))))
+      vars)
+    ))
+
+(defn percents [maps]
+  (let [without-filenames (map #(dissoc % :filename) maps)]
+    (->> without-filenames
+         (apply merge-with (partial merge-with +'))
+         percents-of-single-pack)))
+
 
 
 (defn swi-packs-complete []
@@ -105,8 +146,8 @@
                    "useful-results/swi-pack-types-20000-12"
                    ]
         percents-v2 (->> file-list
-                         (map analysis-v2)
-                         (map #(map (comp percents-of-single-pack :data) %))
+                         (map (partial analysis-v2 false))
+                         (map #(map (comp :vars percents-of-single-pack :data) %))
                          (map (partial map vals))
                          (map (partial map (partial map #(if (float? %) (format "%.2f" %) (str %)))))
                          (map (partial map (partial clojure.string/join ";")))
@@ -115,8 +156,8 @@
                          (clojure.string/join "\n")
                          )
         percents-v3 (->> file-list
-                         (map analysis-v3)
-                         (map #(map (comp percents-of-single-pack :data) %))
+                         (map (partial analysis-v2 true))
+                         (map #(map (comp :vars percents-of-single-pack :data) %))
                          (map (partial map vals))
                          (map (partial map (partial map #(if (float? %) (format "%.2f" %) (str %)))))
                          (map (partial map (partial clojure.string/join ";")))
@@ -127,24 +168,6 @@
         ]
     (spit "useful-results/table-v2.csv" percents-v2)
     (spit "useful-results/table-v3.csv" percents-v3)
-    (hash-map :v2 (map (partial percents analysis-v2) file-list)
-              :v3 (map (partial percents analysis-v3) file-list))
-    ))
-
-
-(defn prob-type-analysis [path]
-  (let [m (->> path
-               get-data
-               (remove string?)
-               (take-nth 2)
-               (remove empty?)
-               (mapcat vals)
-               (map #(if (= :any (:record-type %)) :any :not-any))
-               frequencies)
-        total (apply +' (vals m))]
-    (-> m
-        (assoc :total total)
-        (update :any #(float (/ % total)))
-        (update :not-any #(float (/ % total)))
-        )
+    (hash-map :v2 (map (partial percents analysis-v2 false) file-list)
+              :v3 (map (partial percents analysis-v2 true) file-list))
     ))
