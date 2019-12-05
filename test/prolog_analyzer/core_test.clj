466cbbdebea00868e2f931bbc1f59e6bab82285a
Isabel Wingen <isabel.wingen@raytion.com>
2019-12-05 20:58:02 +0100

    Move built-in test to core_test.clj

--- test/prolog_analyzer/core_test.clj
+++ test/prolog_analyzer/core_test.clj
@@ -1,3 +1,100 @@
 (ns prolog-analyzer.core-test
   (:require [prolog-analyzer.core :as sut]
+            [clojure.java.io :as io]
+            [prolog-analyzer.records :as r]
+            [prolog-analyzer.record-utils :as ru]
             [midje.sweet :refer :all]))
+
+;; Test Build Ins
+
+(def BUILT-IN "edns/builtins.edn")
+(def TMP "resources/tmp.pl")
+(def PREAMBLE ":- module(tmp,[]).\n\n")
+
+(defn analyze-snippet [snippet]
+  (spit (io/file TMP) (str PREAMBLE snippet))
+  (sut/run TMP))
+
+(defn transform-to-types [data [_ _ arity :as pred-id]]
+  (let [res (->> (get-in data [:post-specs pred-id])
+                 last
+                 :conclusion
+                 (map (partial sort-by :id))
+                 (map (partial map :type))
+                 (apply map vector)
+                 (map set)
+                 (map r/->OneOfSpec)
+                 (map ru/simplify)
+                 (map r/to-string)
+                 (interleave (range arity))
+                 (apply hash-map))]
+    (Thread/sleep 1000)
+    res))
+
+(facts
+ "Simple types"
+ (fact "Atom"
+  (-> "foo(X) :- atom(X)."
+      analyze-snippet
+      (transform-to-types ["tmp" "foo" 1]))
+  => (contains {0 "Atom"}))
+ (fact "Atomic"
+       (-> "foo(X) :- atomic(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Atomic"})
+       )
+ (fact "Callable"
+       (-> "foo(X) :- callable(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "OneOf(Atom, Compound)"})
+       )
+ (fact "Compound"
+       (-> "foo(X) :- compound(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Compound"})
+       )
+
+ (fact "Float"
+       (-> "foo(X) :- float(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Float"})
+       )
+
+ (fact "Ground"
+       (-> "foo(X) :- ground(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Ground"})
+       )
+
+ (fact "Integer"
+       (-> "foo(X) :- integer(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Integer"})
+       )
+
+ (fact "Number"
+       (-> "foo(X) :- number(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Number"})
+       )
+ (fact "Nonvar"
+       (-> "foo(X) :- nonvar(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Nonvar"})
+       )
+
+ (fact "Var"
+       (-> "foo(X) :- var(X)."
+           analyze-snippet
+           (transform-to-types ["tmp" "foo" 1]))
+       => (contains {0 "Var"})
+       )
+ )
